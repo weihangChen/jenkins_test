1e9c8496f1c362e4e2327c37df4aeceef313051e
wech <weihangchen@gmail.com>
2018-06-20 06:59:32 +0200

    add another project

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,6 +6,8 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            var tmp = new ClassLibrary1.Class1();
+            tmp.Fire();
             Console.WriteLine("Hello World 3!");
         }
     }
