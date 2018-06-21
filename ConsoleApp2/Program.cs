a9ef7d34aef19978ee7c1755e22627c309f89f74
wech <weihangchen@gmail.com>
2018-06-21 12:49:05 +0200

    add one project

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tmp = new ClassLibrary1.Class1();
+            tmp.Fire();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
